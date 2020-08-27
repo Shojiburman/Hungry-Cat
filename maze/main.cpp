bb5e1dd88c2c9a864ec5d92dd77139478f160bbb
unknown <shojiburman@gmail.com>
2020-08-27 23:35:39 +0600

    initial files

--- maze/main.cpp
+++ maze/main.cpp
@@ -14,7 +14,7 @@ GLfloat spe = 0.1f;
 void updateNew(int value) {
 
     if(pos <-1.0)
-        pos = 1.0f;
+        pos = .5f;
 
     pos -= spe;
 
@@ -558,178 +558,108 @@ void display(void){
     glVertex2f(34, 43);
     glEnd();
 
+    /*---------------CAT---------------------------------------------------*/
+
     glPushMatrix();
 
     glTranslatef(position, positionup, 0.0f);
 
     glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(0, 4);
-    glVertex2f(0, 7);
-    glVertex2f(1, 7);
-    glVertex2f(1, 4);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(1, 3);
-    glVertex2f(1, 8);
-    glVertex2f(2, 8);
-    glVertex2f(2, 3);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
+    glColor3f(.9, 0.1, .5);
     glVertex2f(2, 2);
-    glVertex2f(2, 5);
-    glVertex2f(3, 5);
-    glVertex2f(3, 2);
+    glVertex2f(2, 6);
+    glVertex2f(6, 6);
+    glVertex2f(6, 2);
     glEnd();
 
     glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(6, 2.5);
+    glVertex2f(6, 6.5);
+    glVertex2f(6.5, 6.5);
+    glVertex2f(6.5, 2.5);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(6.5, 3);
+    glVertex2f(6.5, 5.5);
+    glVertex2f(7, 5.5);
+    glVertex2f(7, 3);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(7, 3.5);
+    glVertex2f(7, 5);
+    glVertex2f(7.5, 5);
+    glVertex2f(7.5, 3.5);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(.9, 0.1, .5);
     glVertex2f(2, 6);
-    glVertex2f(2, 9);
-    glVertex2f(3, 9);
+    glVertex2f(2, 7);
+    glVertex2f(3, 7);
     glVertex2f(3, 6);
     glEnd();
 
     glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(3, 2);
-    glVertex2f(3, 4);
-    glVertex2f(4, 4);
-    glVertex2f(4, 2);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(2, 7);
+    glVertex2f(2, 7.5);
+    glVertex2f(2.5, 7.5);
+    glVertex2f(2.5, 7);
     glEnd();
 
     glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(4, 0);
-    glVertex2f(4, 2);
-    glVertex2f(7, 2);
-    glVertex2f(7, 0);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(3, 6);
+    glVertex2f(3, 6.5);
+    glVertex2f(3.5, 6.5);
+    glVertex2f(3.5, 6);
     glEnd();
 
     glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(7, 0);
-    glVertex2f(7, 1);
-    glVertex2f(8, 1);
-    glVertex2f(8, 0);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(4, 2);
-    glVertex2f(4, 12);
-    glVertex2f(12, 12);
-    glVertex2f(12, 2);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(9, 0);
-    glVertex2f(9, 2);
-    glVertex2f(12, 2);
-    glVertex2f(12, 0);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(12, 0);
-    glVertex2f(12, 1);
-    glVertex2f(13, 1);
-    glVertex2f(13, 0);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(12, 5);
-    glVertex2f(12, 13);
-    glVertex2f(13, 13);
-    glVertex2f(13, 5);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(13, 6);
-    glVertex2f(13, 11);
-    glVertex2f(14, 11);
-    glVertex2f(14, 6);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(14, 7);
-    glVertex2f(14, 10);
-    glVertex2f(15, 10);
-    glVertex2f(15, 7);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(4, 12);
-    glVertex2f(4, 14);
-    glVertex2f(6, 14);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(5, 6);
+    glVertex2f(5, 7);
     glVertex2f(6, 7);
+    glVertex2f(6, 6);
     glEnd();
 
     glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(4, 14);
-    glVertex2f(4, 15);
-    glVertex2f(5, 15);
-    glVertex2f(5, 14);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(6, 12);
-    glVertex2f(6, 13);
-    glVertex2f(7, 13);
-    glVertex2f(7, 12);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(10, 12);
-    glVertex2f(10, 14);
-    glVertex2f(12, 14);
-    glVertex2f(12, 12);
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(10, 14);
-    glVertex2f(10, 15);
-    glVertex2f(11, 15);
-    glVertex2f(11, 14);
+    glColor3f(.9, 0.1, .5);
+    glVertex2f(5, 7);
+    glVertex2f(5, 7.5);
+    glVertex2f(5.5, 7.5);
+    glVertex2f(5.5, 7);
     glEnd();
 
     /*---------------eye nose-----------------*/
 
     glBegin(GL_POLYGON);
     glColor3f(1.5, 1.1, 0.5);
-    glVertex2f(8, 9);
-    glVertex2f(8, 10);
-    glVertex2f(9, 10);
-    glVertex2f(9, 9);
+    glVertex2f(4, 4.5);
+    glVertex2f(4, 5);
+    glVertex2f(4.5, 5);
+    glVertex2f(4.5, 4.5);
     glEnd();
 
     glBegin(GL_POLYGON);
     glColor3f(1.5, .5, 0.0);
-    glVertex2f(11, 7);
-    glVertex2f(11, 8);
-    glVertex2f(12, 8);
-    glVertex2f(12, 7);
+    glVertex2f(5.5, 3.5);
+    glVertex2f(5.5, 4);
+    glVertex2f(6, 4);
+    glVertex2f(6, 3.5);
     glEnd();
 
     glBegin(GL_POLYGON);
     glColor3f(1.5, 1.1, 0.5);
-    glVertex2f(12, 9);
-    glVertex2f(12, 10);
-    glVertex2f(13, 10);
-    glVertex2f(13, 9);
+    glVertex2f(6, 4.5);
+    glVertex2f(6, 5);
+    glVertex2f(6.5, 5);
+    glVertex2f(6.5, 4.5);
     glEnd();
 
     glPopMatrix();
